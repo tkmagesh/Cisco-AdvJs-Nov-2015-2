e2fe0fdbee8931bf84ece5a8a39ed9a637c002c5
tkmagesh <tkmagesh77@gmail.com>
2015-11-16 16:13:31 +0530

    Day-1 Complete

---
+++ Day-01/02-Closure/spinner.js
@@ -0,0 +1,16 @@
+create an object and assign it to a variable "spinner"
+
+var spinner = ....
+
+the object is expected to exhibit the following behavior
+
+spinner.up() // => 1
+spinner.up() // => 2
+spinner.up() // => 3
+spinner.up() // => 4
+
+spinner.down() // => 3
+spinner.down() // => 2
+spinner.down() // => 1
+spinner.down() // => 0
+spinner.down() // => -1
